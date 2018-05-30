109a657f9b6eff831a191ec72178c3fa851a6044
Luiz Silva <robertolg.luiz@gmail.com>
2018-05-29 23:07:11 -0300

    Add product and associations with category

--- src/main/java/com/lrsilva/projetospring/ProjetoSpringApplication.java
+++ src/main/java/com/lrsilva/projetospring/ProjetoSpringApplication.java
@@ -1,12 +1,46 @@
 package com.lrsilva.projetospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lrsilva.projetospring.domain.Category;
+import com.lrsilva.projetospring.domain.Product;
+import com.lrsilva.projetospring.repositories.CategoryRepository;
+import com.lrsilva.projetospring.repositories.ProductRepository;
+
 @SpringBootApplication
-public class ProjetoSpringApplication {
+public class ProjetoSpringApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoryRepository categoryRepository;
+	@Autowired
+	ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "IT");
+		Category cat2 = new Category(null, "Office");
+
+		Product p1 = new Product(null, "Computer", 2000.00);
+		Product p2 = new Product(null, "Printer", 800.00);
+		Product p3 = new Product(null, "Mouse", 80.00);
+
+		cat1.getProducts().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProducts().addAll(Arrays.asList(p2));
+		
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1));
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 }
